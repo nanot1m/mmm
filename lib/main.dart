c0de595734c707c1c488a303789418077c70d77b
hazam <hazam@yandex-team.ru>
2019-07-20 13:51:50 +0500

    Внедрили Календарь

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,22 @@ import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State {
+  DateTime _selectedDate = DateTime.now();
+
+  _handleDateSelect(DateTime date, List list) {
+    setState(() {
+      _selectedDate = date;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,11 +33,35 @@ class MyApp extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[Text('Hello World')],
+              children: <Widget>[
+                Text(_selectedDate.toString()),
+                calendar(_handleDateSelect, _selectedDate)
+              ],
             ),
           ),
         ),
       ),
     );
   }
+}
+
+Widget calendar(dynamic Function(DateTime date, List list) onDayPressed,
+    DateTime selectedDateTime) {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 16.0),
+    child: CalendarCarousel(
+      onDayPressed: onDayPressed,
+      weekendTextStyle: TextStyle(
+        color: Colors.red,
+      ),
+      thisMonthDayBorderColor: Colors.grey,
+      weekFormat: false,
+//      markedDatesMap: _markedDateMap,
+      height: 420.0,
+      selectedDateTime: selectedDateTime,
+      daysHaveCircularBorder: false,
+
+      /// null for not rendering any border, true for circular border, false for rectangular border
+    ),
+  );
 }
